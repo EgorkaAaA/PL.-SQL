9b6819ed564598cff22147a5d897fa953df6ccef
Егор Лебедев <m123ry132@gmail.com>
2021-10-28 18:26:52 +0700

    record

--- thirdTask.sql
+++ thirdTask.sql
@@ -135,14 +135,43 @@ begin
     v_id_doctor := 1;
     if v_id_doctor is null then
         for i in (select * from LEBEDEV_EG.DOCTOR_TIMETABLE dt
-where dt.TIME_SPACES_TO < sysdate and dt.TIME_SPACES_FROM < sysdate)
+    where dt.TIME_SPACES_TO < sysdate and dt.TIME_SPACES_FROM < sysdate)
         loop
             DBMS_OUTPUT.PUT_LINE(i.ID_DOCTOR);
             end loop;
         else for i in (select * from LEBEDEV_EG.DOCTOR_TIMETABLE dt
-where dt.ID_DOCTOR = v_id_doctor and dt.TIME_SPACES_TO < sysdate and dt.TIME_SPACES_FROM < sysdate)
+    where dt.ID_DOCTOR = v_id_doctor and dt.TIME_SPACES_TO < sysdate and dt.TIME_SPACES_FROM < sysdate)
         loop
             DBMS_OUTPUT.PUT_LINE(i.ID_DOCTOR);
             end loop;
     end if;
 end;
+
+declare
+    cursor1 sys_refcursor;
+    type v_record_1 is record(
+        v_doc_count number,
+        v_name_hosp varchar2(100),
+        v_id_spec number,
+        v_is_open number
+                             );
+begin
+    open cursor1 for
+        SELECT count(D.ID_DOCTOR) as count, H.NAME_HOSPITAL, DS.ID_SPECIALITY,H.IS_OPEN  from HOSPITAL H
+            inner join LEBEDEV_EG.DOCTOR D
+            on H.ID_HOSPITAL = D.ID_HOSPITAL
+            inner join LEBEDEV_EG.DOCTOR_SPECIALITY DS
+            on DS.ID_DOCTOR = D.ID_DOCTOR
+            where DS.ID_SPECIALITY = 10 and H.DELETED is null and D.DELETED is null
+            group by H.ID_HOSPITAL, H.NAME_HOSPITAL,H.IS_OPEN,H.ID_HOSPITAL_TAPE, DS.ID_SPECIALITY
+            order by case when H.ID_HOSPITAL_TAPE = (select HT.ID_HOSPITAL_TAPE from HOSPITAL_TAPE HT where HT.NAME_HOSPITAL_TAPE = 'Государственная')
+                        then 0
+                        else 1
+                    end desc,
+                     count(D.ID_DOCTOR) desc,
+                    H.IS_OPEN desc;
+    fetch cursor1 into v_record_1;
+    DBMS_OUTPUT.PUT_LINE(v_record_1.v_name_hosp);
+    close cursor1;
+end;
+
